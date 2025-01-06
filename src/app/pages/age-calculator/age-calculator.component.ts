8eeb795b58dd2c01e74da68184e35ea7d140b427
Ekaterina Shobanova <k.shobanova@yandex.ru>
2025-01-06 16:46:00 +0300

    Initial commit

---
+++ src/app/pages/age-calculator/age-calculator.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {RouterLink} from "@angular/router";
+
+@Component({
+  selector: 'app-age-calculator',
+    imports: [
+        RouterLink
+    ],
+  templateUrl: './age-calculator.component.html',
+  styleUrl: './age-calculator.component.css'
+})
+export class AgeCalculatorComponent {
+
+}
